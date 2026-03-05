2366872a9b13f9878905167ceab48880239c0aab
장강민 <jkm9780533@gmail.com>
2026-03-05 13:57:00 +0900

    variable ex

---
+++ src/variable/ex/VaeEx3.java
@@ -0,0 +1,13 @@
+package variable.ex;
+
+public class VaeEx3 {
+    public static void main(String[] args) {
+        // long 타입의 변수를 선언하고, 그 변수를 10000000000(백억)으로 초기화한 후 출력하세요
+        long num1 = 10000000000L;
+        System.out.println(num1);
+
+        // boolean 타입의 변수를 선언하고, 그 변수를 true 로 초기화한후 출력하세요
+        boolean bool = true;
+        System.out.println(bool);
+    }
+}
