9c096910b0efe8f0d146104bb84a0f2629bf8f74
장강민 <jkm9780533@gmail.com>
2026-03-05 14:00:51 +0900

    Operator1

---
+++ src/operator/Operator1.java
@@ -0,0 +1,20 @@
+package operator;
+
+public class Operator1 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 2;
+
+        int sum = a + b;
+        int diff = a - b;
+        int multi = a * b;
+        int div = a / b;
+        int mod = a % b;
+
+        System.out.println(sum);
+        System.out.println(diff);
+        System.out.println(multi);
+        System.out.println(diff);
+        System.out.println(mod);
+    }
+}
