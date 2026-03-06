1d97584f3c8953a5abb4d371fc5d29cdcd5caf05
장강민 <jkm9780533@gmail.com>
2026-03-06 09:17:21 +0900

    Ternary operator

---
+++ src/operator/Ternary1.java
@@ -0,0 +1,9 @@
+package operator;
+
+public class Ternary1 {
+    public static void main(String[] args) {
+        int num1 = 5, num2 = 10;
+        int result = num1 - num2 > 0 ? num1 : num2;
+        System.out.println("두 정수 중 더 큰 수는 "+result+"입니다.");
+    }
+}
