439fed47e05b4b500d6869a7f25dfaf7fe61b1f2
장강민 <jkm9780533@gmail.com>
2026-03-07 21:28:32 +0900

    Switch1, 2

--- src/cond/Switch2.java
+++ src/cond/Switch2.java
@@ -3,18 +3,11 @@ package cond;
 public class Switch2 {
     public static void main(String[] args) {
         int grade = 2;
-        int coupon;
-        switch (grade) {
-            case 1:
-                coupon = 1000;
-                break;
-            case 2:
-            case 3:
-                coupon = 3000;
-                break;
-            default:
-                coupon = 500;
-        }
+        int coupon = switch (grade) {
+            case 1 -> 1000;
+            case 2, 3 -> 3000;
+            default -> 500;
+        };
 
         System.out.println("발급받은 쿠폰 " + coupon);
     }
