85cd73b5444498939f116922c99f155ca2af5f78
장강민 <jkm9780533@gmail.com>
2026-03-11 15:02:21 +0900

    roop

---
+++ src/roop/DoWhile1.java
@@ -0,0 +1,11 @@
+package roop;
+
+public class DoWhile1 {
+    public static void main(String[] args) {
+        int i = 10;
+        do {
+            System.out.println("한재 숫자는: " + i);
+            i++;
+        } while (i < 3);
+    }
+}
