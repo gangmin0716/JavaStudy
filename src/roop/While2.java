f2c876f75b753cb7acfd8ce99c7fadfae5f4505f
장강민 <jkm9780533@gmail.com>
2026-03-11 14:49:40 +0900

    while 1,2

---
+++ src/roop/While2.java
@@ -0,0 +1,26 @@
+package roop;
+
+public class While2 {
+    public static void main(String[] args) {
+        int a = 0;
+        while(a < 10) {
+            System.out.println(a + " ");
+            a++;
+        }
+
+        int b = 10;
+        while (b > 0) {
+            System.out.println(b + " ");
+            b--;
+        }
+
+        int c = 0;
+        while(true) {
+            if (c > 10) {
+                break;
+            }
+            System.out.println(c + " ");
+            c++;
+        }
+    }
+}
