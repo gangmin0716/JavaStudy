0596f3c783c93829c42bdbaa7cb694a3021dcee0
장강민 <jkm9780533@gmail.com>
2026-03-18 13:53:55 +0900

    Scanner

---
+++ src/scanner/ScannerEx1.java
@@ -0,0 +1,18 @@
+package scanner;
+import java.util.Scanner;
+
+public class ScannerEx1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        int num3 = sc.nextInt();
+
+        int sum = num1 + num2 + num3;
+        int avg = sum / 3;
+
+        System.out.println(sum);
+        System.out.println(avg);
+    }
+}
