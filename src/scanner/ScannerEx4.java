68cb0db9dc47159e6320206e130eca29f8f10681
장강민 <jkm9780533@gmail.com>
2026-03-18 14:10:25 +0900

    Scanner 4

---
+++ src/scanner/ScannerEx4.java
@@ -0,0 +1,19 @@
+package scanner;
+import java.util.Scanner;
+
+public class ScannerEx4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while(true) {
+            int num1 = sc.nextInt();
+            int num2 = sc.nextInt();
+
+            int sum = num1 + num2;
+            if (num1 == 0 && num2 == 0){
+                break;
+            }
+            System.out.println(sum);
+        }
+    }
+}
