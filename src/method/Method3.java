9c53f655c7f69cb38d70655f3dceed24b045c6b9
장강민 <jkm9780533@gmail.com>
2026-03-25 15:19:29 +0900

    method

---
+++ src/method/Method3.java
@@ -0,0 +1,19 @@
+package method;
+
+public class Method3 {
+    public static void main(String[] args) {
+        printHeader();
+        System.out.println("프로그램이 동작합니다.");
+        printFooter();
+    }
+
+    public static void printHeader(){
+        System.out.println("= 프로그래밍을 시작합니다 = ");
+        // return void 경우 생략 ㄱㄴ
+    }
+
+    public static void  printFooter(){
+        System.out.println("= 프로그램을 종료합니다 =");
+    }
+
+}
