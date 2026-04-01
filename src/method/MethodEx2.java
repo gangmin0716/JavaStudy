986798a5dd25af22e6f2ded1bb89154c1cf21db9
장강민 <jkm9780533@gmail.com>
2026-04-01 13:51:44 +0900

    .

---
+++ src/method/MethodEx2.java
@@ -0,0 +1,18 @@
+package method;
+
+public class MethodEx2 {
+
+    public static void printMessage(String message, int count) {
+        for (int i = 0; i < count; i++) {
+            System.out.println(message);
+        }
+    }
+
+    public static void main(String[] args) {
+        String message = "Hello, world!";
+
+        printMessage(message, 3);
+        printMessage(message, 5);
+        printMessage(message, 7);
+    }
+}
