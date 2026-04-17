f23cc4ef8d5135f60f8f50c6be9e5a07597a17c2
장강민 <jkm9780533@gmail.com>
2026-04-17 09:56:13 +0900

    Override

---
+++ src/overrriding/CarMain.java
@@ -0,0 +1,12 @@
+package overrriding;
+
+import extends2.GasCar;
+
+public class CarMain {
+    public static void main(String[] args) {
+        ElectricCar electricCar = new ElectricCar();
+        electricCar.move();
+        GasCar gasCar = new GasCar();
+        gasCar.move();
+    }
+}
