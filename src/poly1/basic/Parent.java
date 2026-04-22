81300aad33464ddc55074973b530a724f4ace156
장강민 <jkm9780533@gmail.com>
2026-04-22 14:00:14 +0900

    다형성

---
+++ src/poly1/basic/Parent.java
@@ -0,0 +1,7 @@
+package poly1.basic;
+
+public class Parent {
+    public void parentMethod() {
+        System.out.println("Parent.parentMethod");
+    }
+}
---
+++ src/poly1/basic/PolyMain.java
@@ -0,0 +1,20 @@
+package poly1.basic;
+
+import super2.C;
+
+public class PolyMain {
+    public static void main(String[] args) {
+        System.out.println("Parent -> Parent");
+        Parent parent = new Parent();
+        parent.parentMethod();
+
+        System.out.println("Child -> Child");
+        Child child = new Child();
+        child.parentMethod();
+        child.childMethod();
+
+        System.out.println("Parent -> Child");
+        Parent poly = new Child();
+        poly.parentMethod();
+    }
+}
