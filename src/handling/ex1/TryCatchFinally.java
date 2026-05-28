036b34ebab40df5b58180bae2dc9aa69354e1179
장강민 <jkm9780533@gmail.com>
2026-05-28 13:55:38 +0900

    예외처리 핸들링

---
+++ src/handling/ex1/TryCatchFinally.java
@@ -0,0 +1,13 @@
+package handling.ex1;
+
+public class TryCatchFinally {
+    public static void main(String[] args) {
+        try {
+            System.out.println(3 / 0);
+        } catch (ArithmeticException e) {
+            System.out.println("숫자는 0으로 나눌 수 없습니다.");
+        } finally {
+            System.out.println("프로그램 종료");
+        }
+    }
+}
