0d9ad3df6f895ecc90c79febd39c077af452fa74
장강민 <jkm9780533@gmail.com>
2026-05-28 13:50:20 +0900

    예외처리

--- src/exception/ex1/CheckedException.java
+++ src/exception/ex1/CheckedException.java
@@ -1,32 +1,32 @@
-package exception.ex1;
-
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
-
-class A {
-    protected Object clone() throws CloneNotSupportedException {
-        return super.clone();
-    }
-}
-
-public class CheckedException {
-    public static void main(String[] args) {
-
-        // InterruptedException
-        Thread.sleep(1000);
-
-        // ClassNotFoundException
-        Class cls = Class.forName("java.lang.Object");
-
-        // IOException
-        InputStreamReader in = new InputStreamReader(System.in);
-        in.read();
-
-        // FileNotFoundException
-        FileInputStream fis = new FileInputStream("text.txt");
-
-        // CloneNotSuppotedException
-        A a1 = new A();
-        A a2 = (A) a1.clone();
-    }
-}
+//package exception.ex1;
+//
+//import java.io.FileInputStream;
+//import java.io.InputStreamReader;
+//
+//class A {
+//    protected Object clone() throws CloneNotSupportedException {
+//        return super.clone();
+//    }
+//}
+//
+//public class CheckedException {
+//    public static void main(String[] args) {
+//
+//        // InterruptedException
+//        Thread.sleep(1000);
+//
+//        // ClassNotFoundException
+//        Class cls = Class.forName("java.lang.Object");
+//
+//        // IOException
+//        InputStreamReader in = new InputStreamReader(System.in);
+//        in.read();
+//
+//        // FileNotFoundException
+//        FileInputStream fis = new FileInputStream("text.txt");
+//
+//        // CloneNotSuppotedException
+//        A a1 = new A();
+//        A a2 = (A) a1.clone();
+//    }
+//}
