20e2e866b488e50aa30f4eea6385a7c1fd17de9d
장강민 <jkm9780533@gmail.com>
2026-09-04 10:22:42 +0900

    Restore JavaStudy to original pre-merge state

--- SpringStudy/school/src/main/java/com/dgsw/school/repository/EnrollRepository.java
+++
@@ -1,7 +0,0 @@
-package com.dgsw.school.repository;
-
-import com.dgsw.school.entity.EnrollEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EnrollRepository extends JpaRepository<EnrollEntity, String> {
-}
